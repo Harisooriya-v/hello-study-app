9c9a5ae0039b7f0c7eeb3fb9e8850b6bc1676c1c
Harisooriya <harisooriyav@gmail.com>
2026-05-29 10:43:40 +0530

    test: update unit and screenshot tests
    Update app name assertion and replace greeting component with HelloStudyTopBar in screenshot tests.

--- app/src/test/java/com/example/GreetingScreenshotTest.kt
+++ app/src/test/java/com/example/GreetingScreenshotTest.kt
@@ -21,7 +21,7 @@ class GreetingScreenshotTest {
 
   @Test
   fun greeting_screenshot() {
-    composeTestRule.setContent { MyApplicationTheme { Greeting("Robolectric") } }
+    composeTestRule.setContent { MyApplicationTheme { HelloStudyTopBar(isDark = true, onThemeToggle = {}) } }
 
     composeTestRule.onRoot().captureRoboImage(filePath = "src/test/screenshots/greeting.png")
   }
